d84c26fe615e1207093995b14f7462ebded45fca
Nishanth21 <nishanthcoder21@gmail.com>
2025-07-26 00:41:02 +0530

    just tried to push to new branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="text-4xl font-bold text-center text-green-500 transition duration-1000 hover:text-blue-500 transform hover:scale-110 ">
-      Hello World
-      <Button variant="custom" className="ml-10">
-        Click Me
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-4xl font-bold text-center text-green-500 transition duration-1000 hover:text-blue-500 transform hover:scale-110 mb-6">
+      Welcome to MeetAI
+      </div>
+      <Button variant="outline" className="px-6 py-3 text-lg font-medium hover:bg-blue-500 hover:text-white transition">
+      Get Started
       </Button>
     </div>
   );
