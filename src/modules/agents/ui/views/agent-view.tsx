830565025a33c995177e1a30a299098e6a9d06ad
Nishanth21 <nishanthcoder21@gmail.com>
2025-08-25 22:02:24 +0530

    12: agents-data-table

--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -2,28 +2,22 @@
 
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
-import { ResponsiveDialog } from "@/components/responsive-dialog";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
   
   return (
-    <div>
-      <p>{JSON.stringify(data, null, 2)}</p>
-      <ResponsiveDialog
-        title="Agents"
-        description="List of all agents"
-        open={false}
-        onOpenChange={() => {}}
-      >
-        <button className="mb-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
-          Create New Agent
-        </button>
-        {JSON.stringify(data, null, 2)}
-      </ResponsiveDialog>
+    <div className="px-4 flex-1 md:px-8 flex flex-col gap-y-4">
+      <DataTable columns={columns} data={data} onRowClick={(row) => console.log(row)} />
+        {data?.length === 0 && (
+          <EmptyState title="Create your first agent" description="Create an agent to join your meeting. Each agent will follow your instructions and can interact with partcipants during the call." />
+        )}
     </div>
   );
 };
